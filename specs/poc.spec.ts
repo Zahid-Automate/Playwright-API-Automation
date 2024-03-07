666d2735c47a192b5662efb7e57dc955521f79da
mzahid <abdulzahid.01@gmail.com>
2024-03-07 13:50:54 -0800

    include query param test case

--- specs/poc.spec.ts
+++ specs/poc.spec.ts
@@ -4,9 +4,17 @@ const request = supertest('https://jsonplaceholder.typicode.com');
 describe('POC Tests',()=>{
           it('GET /posts', async () => {
           const res = await request.get('/posts');
-          console.log(res);
+         // console.log(res);
           expect(res.statusCode).toBe(200)
           expect(res.body[1].id).toBe(2)
          
         })
+        // Test GET with queryParam
+         it('GET /comments', async()=>{
+          const res = await request
+          .get('/comments')
+          .query({postId:1, id:1})
+          expect(res.body[0].postId).toBe(1)
+          console.log(res)
+         })
     })
