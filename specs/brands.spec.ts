1a5ec7250abb3ccf4c6f5a3f75747ae289d1634e
mzahid <abdulzahid.01@gmail.com>
2024-03-13 01:30:37 -0700

    Implemented GET /brands/{id}

--- specs/brands.spec.ts
+++ specs/brands.spec.ts
@@ -6,7 +6,15 @@ describe('Brands Tests', () => {
         const res = await request.get('/brands');
         expect(res.statusCode).toBe(200);
         expect(res.body.length).toBeGreaterThan(100);
-        expect(Object.keys(res.body[0])).toEqual(['_id','name'])
-      })
+        expect(Object.keys(res.body[0])).toEqual(['_id', 'name'])
+    })
+
+    it.only('GET /brands/{id}', async () => {
+        const res = await request.get('/brands/65d1b173986188d4dce446ad');
+        expect(res.statusCode).toBe(200);
+        console.log(res);
+        expect(Object.keys(res.body)).toEqual(['_id', 'name','description','createdAt','updatedAt','__v']);
+        expect(res.body.name).toBe('zahid');
+    })
 
 })
