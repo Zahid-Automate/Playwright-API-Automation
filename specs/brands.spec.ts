c7c257325b7b85c840c3fc3ca0acff0b642f785f
mzahid <abdulzahid.01@gmail.com>
2024-03-18 00:21:45 -0700

    Implement describe block for POST

--- specs/brands.spec.ts
+++ specs/brands.spec.ts
@@ -1,7 +1,7 @@
 import * as supertest from 'supertest';
 const request = supertest('https://practice-react.sdetunicorns.com/api/test');
 
-describe('Brands Tests', () => {
+describe('GET Brands Tests', () => {
     it('GET /brands', async () => {
         const res = await request.get('/brands');
         expect(res.statusCode).toBe(200);
@@ -9,7 +9,7 @@ describe('Brands Tests', () => {
         expect(Object.keys(res.body[0])).toEqual(['_id', 'name'])
     })
 
-    it.only('GET /brands/{id}', async () => {
+    it('GET /brands/{id}', async () => {
         const res = await request.get('/brands/65d1b173986188d4dce446ad');
         expect(res.statusCode).toBe(200);
         console.log(res);
@@ -17,4 +17,31 @@ describe('Brands Tests', () => {
         expect(res.body.name).toBe('zahid');
     })
 
+    it('GET /brands/{id}', async () => {
+        const res = await request.get('/brands/65d1b173986188d4dce446ad');
+        expect(res.statusCode).toBe(200);
+        console.log(res);
+        expect(Object.keys(res.body)).toEqual(['_id', 'name','description','createdAt','updatedAt','__v']);
+        expect(res.body.name).toBe('zahid');
+    })   
+    
+
+})
+
+describe('Post Brands Test',()=>{
+    it.only('POST /brands', async () => {
+        const req = {
+            "name": "GoPro Camera Small",
+            "description": "Camera Small Brands"
+          }
+        const res = await request
+          .post('/brands')
+          .send(req)  ;
+
+        expect(res.statusCode).toEqual(200);
+        expect(res.body.name).toBe(req.name);
+        expect(res. body).toHaveProperty('createdAt')
+        expect(res.body.description).toBe(req.description);
+        
+    })
 })
