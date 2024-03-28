2355f0e575fc6f5df6db71cb351b3a81fece359e
mzahid <abdulzahid.01@gmail.com>
2024-03-28 15:00:02 -0700

    Include email and password parameters

--- config/base.config.ts
+++ config/base.config.ts
@@ -1,3 +1,5 @@
 export default{
-    baseUrl:'https://practice-react.sdetunicorns.com/api/test'
+    baseUrl:'https://practice-react.sdetunicorns.com/api/test',
+    email: "mod@mail.com",
+    password:"Modpass123!"
 }
